154a3d53388556d5231f3a5b973373d53cebc872
kagasu <admin@kagasu.net>
2018-03-16 15:10:49 +0900

    support auto delete old backup

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import MySQLdb
 import subprocess
 import os
 import shutil
-from datetime import datetime as dt
+import datetime
+from collections import namedtuple
 
 def GetDatabaseNames(config):
     conn = MySQLdb.connect(user = config.get('db').get('user'), password = config.get('db').get('password'), host = config.get('db').get('host'), charset = 'utf8')
@@ -21,11 +22,33 @@ def GetDatabaseNames(config):
 
     return [x[0] for x in dbNames if x[0] not in config.get('exclue_db_names')]
 
+def GetGdriveDirectories():
+    FileInfo = namedtuple('FileInfo', ('fileId', 'fileName', 'fileCreatedTime'))
+    command = 'gdrive list --order "createdTime asc" -m 100'
+    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    while True:
+        line = proc.stdout.readline()
+        if line:
+            line = line.decode('utf-8').strip().split()
+            fileType = line[2]
+            if fileType == 'dir':
+                yield FileInfo(fileId = line[0], fileName = line[1], fileCreatedTime = line[3])
+        if not line and proc.poll() is not None:
+            break
+
+def DeleteOldBackup(period):
+    time = datetime.datetime.now() - datetime.timedelta(days=period)
+    for file in GetGdriveDirectories():
+        fileCreatedTime = datetime.datetime.strptime(file.fileCreatedTime, '%Y-%m-%d')
+        if time > fileCreatedTime:
+            subprocess.call('gdrive delete {0} -r'.format(file.fileId), shell=True)
+
 def main():
     config = json.load(open('config.json'))
     dbNames = GetDatabaseNames(config)
 
-    dirName = dt.now().strftime('%Y%m%d_%H%M%S')
+    dirName = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
     os.mkdir(dirName)
 
     for dbName in dbNames:
@@ -45,9 +68,11 @@ def main():
         subprocess.call(mysqlDumpCommand, shell=True)
         subprocess.call(encryptCommand, shell=True)
         os.remove('{0}/{1}.gz'.format(dirName, dbName))
-    
-    subprocess.call('drive upload --file {0}'.format(dirName), shell=True)
+
+    subprocess.call('gdrive upload --file {0}'.format(dirName), shell=True)
     shutil.rmtree(dirName)
+
+    DeleteOldBackup(config.get('gdrive').get('period'))
 
 if __name__ == '__main__':
     main()
